8c02379d9fd91338d7b2bce18076cfea268d4188
Ryujongwoo <55866526+Ryujongwoo@users.noreply.github.com>
2020-09-09 10:36:04 +0900

    Add files via upload

--- 20200909_033/AnimationTest2.java
+++ 20200909_033/AnimationTest2.java
@@ -10,7 +10,7 @@ import java.util.Random;
 
 import javax.swing.JPanel;
 
-public class AnimationTest2 extends JPanel {
+public class AnimationTest2 extends JPanel implements Runnable {
 	
 	Image cards;
 	int w = 1027 / 13, h = 615 / 5;		// 카드 1장의 크기
@@ -28,21 +28,51 @@ public class AnimationTest2 extends JPanel {
 	public static void main(String[] args) {
 		
 		Frame window = new Frame("Animation");
-		window.setBounds(800, 100, 1027, 615);
+		window.setBounds(800, 100, 1045, 530);
 		window.addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowClosing(WindowEvent e) {
 				System.exit(0);
 			}
 		});
+		
+		AnimationTest2 animation = new AnimationTest2();
+		window.add(animation);
+		
 		window.setVisible(true);
+		
+		Thread thread = new Thread(animation);
+		thread.start();
 		
 	}
 
 	@Override
-	public void paint(Graphics arg0) {
-		// TODO Auto-generated method stub
-		super.paint(arg0);
+	public void paint(Graphics g) {
+		
+//		g.drawImage(cards, 0, 0, this);
+		
+		for (int i = 0; i < 1000000; i++) {
+			int r = random.nextInt(51) + 1;
+			int temp = numbers[0];
+			numbers[0] = numbers[r];
+			numbers[r] = temp;
+		}
+		
+		for (int i = 0; i < numbers.length; i++) {
+			g.drawImage(cards, w*(i%13), h*(i/13), w*(i%13+1), h*(i/13+1), 
+					w*(numbers[i]%13), h*(numbers[i]/13), w*(numbers[i]%13+1), h*(numbers[i]/13+1), this);
+		}
+		
+	}
+
+	@Override
+	public void run() {
+		
+		while (true) {
+			try { Thread.sleep(1000); } catch (InterruptedException e) { e.printStackTrace(); }
+			repaint();
+		}
+		
 	}
 
 }
